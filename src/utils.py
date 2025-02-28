171d41c9daf3b38c1c8323e6483d0adb88f0f42b
Ronit Saini <ronittsaini@gmail.com>
2025-02-28 18:46:46 +0530

    few more commits

--- src/utils.py
+++ src/utils.py
@@ -16,7 +16,7 @@ def save_obj(file_path,obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, 'wb') as f:
-            dill.save(f, obj)
+            dill.dump(obj,f)
     except Exception as e:
         return CustomException(e,sys)
     
@@ -52,3 +52,11 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e, sys)    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
